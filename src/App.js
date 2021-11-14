3c8a7e04dd171e969cfc55947e46bdfa68d8d947
SabihaShaik <sabihaabc1@gmail.com>
2021-11-13 19:41:47 -0500

    Modify App.js, add .env and incorporate API to react app

--- src/App.js
+++ src/App.js
@@ -1,58 +1,58 @@
 import './App.css';
 import React, { Component } from 'react';
+import axios from 'axios'
+
+require('dotenv').config();
 
 class App extends Component{
 
   constructor(props){
-    super(props);
-    this.state = {
-      items:[], //array of data we want to fetch
-      isLoaded: false //indicates whether items have loaded from Twitter API
-    }
+    super(props)
   }
-
-  componentDidMount(){
-
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(json => {
-          this.setState({
-            isLoaded:true,
-            items: json,
-          })
-      });
-
-  }
-
   render() {
-
-    var { isLoaded, items } = this.state;
-
-    if(!isLoaded){
-
-      return <div>Loading...</div>;
-
-    }
-
-    else{
-
       return (
         <div className="App">
-          
-          <ul>
-            {items.map(item => (
-              <li key = {item.id}>
-                Username: {item.username} | Email: {item.email}
-              </li>
-            ))}
-          </ul>
-
+        
+        <input type="text" id="userText"/>
+            <button id="analyze" onClick={()=> this.getSentimentResult("this is an example")}> Analyze </button>
+            <p> The sentiment is: <span id="sentiment">???</span></p>
         </div>
       );
 
     }
-  }
 
+  getSentimentResult(userText){
+  console.log( process.env.REACT_APP_AZURE_API_KEY)
+      var data = JSON.stringify({
+      "documents": [
+          {
+          "id": "1",
+          "language": "en",
+          "text": userText
+          }
+      ]
+      });
+      
+      var config={
+          url :'https://isthismyfriend.cognitiveservices.azure.com/text/analytics/v3.1/sentiment',
+          method: 'post',
+          headers:{
+              'Content-Type': 'application/json',
+              'Ocp-Apim-Subscription-Key':   process.env.REACT_APP_AZURE_API_KEY
+          },
+          data: data
+      }
+      axios(config)
+      .then(function (response) {
+          var sentimentfromAPI = response.data.documents[0].sentiment;
+          console.log(sentimentfromAPI)
+          
+      })
+      .catch(function (error) {
+          console.log(error);
+      });
+    }
+  
 }
 
 export default App;
